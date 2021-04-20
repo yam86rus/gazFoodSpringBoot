616fd2fa6d18a22069f862524faec7a5309d92b6
Yam86rus <yam86rus@gmail.com>
2021-04-20 17:20:37 +0500

    add myTemplate.html, modify all-employees.html, add model attribute in greeting controller, add routing.js in index.html

--- src/main/java/com/gazfood/gazfoodspringboot/controllers/GreetingController.java
+++ src/main/java/com/gazfood/gazfoodspringboot/controllers/GreetingController.java
@@ -1,18 +1,28 @@
 package com.gazfood.gazfoodspringboot.controllers;
 
+import com.gazfood.gazfoodspringboot.entity.Employee;
+import com.gazfood.gazfoodspringboot.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private EmployeeService employeeService;
 
     @GetMapping("")
-    public String main() {
+    public String main(Model model) {
+        int employeesCount = employeeService.getAllEmployees().size();
+        model.addAttribute("employeesCount", employeesCount);
         return "index";
     }
 
     @GetMapping("/restapi")
-    public String restapi(){
+    public String restapi() {
         return "restapi";
     }
 
---
+++ src/main/resources/static/js/routing.js
@@ -0,0 +1,10 @@
+// возможно потом мне будет стыдно за код ниже :p
+// но пока есть как есть.
+
+let id_employee = document.getElementById("id_employee");
+
+id_employee.onclick = function () {
+    window.location.href = '/employees';
+}
+
+
