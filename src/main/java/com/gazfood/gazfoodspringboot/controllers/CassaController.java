c2ea41bab956c9cd010824271aebea09a31a37cb
yam86rus <yam86rus@gmail.com>
2021-04-24 20:41:42 +0500

    add model casses crud

--- src/main/java/com/gazfood/gazfoodspringboot/controllers/CassaController.java
+++ src/main/java/com/gazfood/gazfoodspringboot/controllers/CassaController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,9 +20,36 @@ public class CassaController {
     private CassaService cassaService;
 
     @GetMapping("/casses")
-    private String showCassaPage(Model model){
+    private String showCassaPage(Model model) {
         List<Cassa> allCasses = cassaService.getAllCasses();
-        model.addAttribute("allCasses",allCasses);
+        model.addAttribute("allCasses", allCasses);
         return "all-casses";
     }
+
+    @RequestMapping("/addNewCassa")
+    public String addNewCassa(Model model) {
+        Cassa cassa = new Cassa();
+        model.addAttribute("cassa", cassa);
+        return "cassa-form";
+    }
+
+    @RequestMapping("saveCassa")
+    public String saveCassa(@ModelAttribute("cassa") Cassa cassa) {
+        cassaService.saveCassa(cassa);
+        return "redirect:casses";
+    }
+
+    @RequestMapping("updateCassa")
+    public String updateCassa(@RequestParam("cassaId") int id,Model model) {
+        Cassa cassa = cassaService.getCassa(id);
+        model.addAttribute("cassa",cassa);
+        return "cassa-form";
+    }
+
+    @RequestMapping("deleteCassa")
+    public String deleteCassa(@RequestParam("cassaId") int id) {
+        cassaService.deleteCassa(id);
+        return "redirect:casses";
+    }
+
 }
