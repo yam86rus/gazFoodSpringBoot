e8b1b9d64d5817843a7539931fc0cfdd26f19ddd
Yam86rus <yam86rus@gmail.com>
2021-07-05 18:39:31 +0500

    add phoneNumberInCabinet entity

---
+++ src/main/java/com/gazfood/gazfoodspringboot/dao/PhoneNumberInCabinetRepository.java
@@ -0,0 +1,6 @@
+package com.gazfood.gazfoodspringboot.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhoneNumberInCabinet extends JpaRepository<PhoneNumberInCabinet, Integer> {
+}
---
+++ src/main/java/com/gazfood/gazfoodspringboot/service/PhoneNumberInCabinetIml.java
@@ -0,0 +1,2 @@
+package com.gazfood.gazfoodspringboot.service;public class PhoneNumberInCabinetIml {
+}
