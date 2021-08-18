f48e8d43861dfbed82ac5785055012030a746fcd
Yam86rus <yam86rus@gmail.com>
2021-08-18 12:23:00 +0500

    Добавил crud для блока 'Инвентаризация'

--- src/main/java/com/gazfood/gazfoodspringboot/controllers/AhoController.java
+++ src/main/java/com/gazfood/gazfoodspringboot/controllers/AhoController.java
@@ -1,26 +1,25 @@
 package com.gazfood.gazfoodspringboot.controllers;
 
-import com.gazfood.gazfoodspringboot.entity.Cassa;
 import com.gazfood.gazfoodspringboot.entity.Inventory;
 import com.gazfood.gazfoodspringboot.entity.PhoneNumberInCabinet;
 import com.gazfood.gazfoodspringboot.entity.User;
-import com.gazfood.gazfoodspringboot.exports.CassaExcelExporter;
 import com.gazfood.gazfoodspringboot.exports.PhoneNumberInCabinetExcelExporter;
 import com.gazfood.gazfoodspringboot.service.InventoryService;
 import com.gazfood.gazfoodspringboot.service.PhoneNumberInCabinetService;
 import com.gazfood.gazfoodspringboot.service.UserService;
-import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Principal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -62,7 +61,7 @@ public class AhoController {
     }
 
     @RequestMapping("/aho/allInventory")
-    private String showAllInventory(Model model, Principal principal){
+    private String showAllInventory(Model model, Principal principal) {
         List<Inventory> allInventory = inventoryService.getAllInventory();
         model.addAttribute("allInventory", allInventory);
 
@@ -70,6 +69,47 @@ public class AhoController {
         model.addAttribute("user", user);
 
         return "all-inventory";
+    }
+
+    @RequestMapping("/aho/addNewInventory")
+    public String addNewInventory(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+
+        Inventory inventory = new Inventory();
+        inventory.setEditingDate(LocalDate.now());
+        inventory.setEditingPerson(user.getUserLastName() + " " + user.getUserName() + " " + user.getPatronymic());
+        model.addAttribute("inventory", inventory);
+        return "inventory-form";
+    }
+
+    @RequestMapping("/aho/saveInventory")
+    public String saveInventory(Inventory inventory) {
+        inventoryService.saveInventory(inventory);
+        return "redirect:/aho/allInventory";
+    }
+
+    @RequestMapping("/aho/deleteInventory")
+    public String deleteInventory(@RequestParam("inventoryId") int id) {
+        inventoryService.deleteInventory(id);
+        return "redirect:/aho/allInventory";
+
+    }
+
+    @RequestMapping("/aho/updateInventory")
+    public String editInventory(@RequestParam("inventoryId") int id, Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+
+        Inventory inventory = inventoryService.getInventory(id);
+        inventory.setEditingDate(LocalDate.now());
+        inventory.setEditingPerson(user.getUserLastName() + " " + user.getUserName() + " " + user.getPatronymic());
+        model.addAttribute("inventory", inventory);
+
+
+
+        return "inventory-form";
+
     }
 
 
