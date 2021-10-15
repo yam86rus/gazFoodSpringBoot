30116c0831c68715b9a1b02c0fd8b89e770db4a7
yam86rus <yam86rus@gmail.com>
2021-10-15 15:56:24 +0500

    Первый pipline

--- scripts/deployProd.sh
+++ scripts/deployProd.sh
@@ -9,5 +9,5 @@ echo 'Start maven package...'
 mvn package
 
 echo 'Copy files to prod...'
-scp /home/maxim/IdeaProjects/gazFoodSpringBoot/target/GazFoodSpringBoot-0.1.0-SNAPSHOT.jar user@169.168.9.251:/home/user/tempToProd
+scp /home/maxim/IdeaProjects/gazFoodSpringBoot/target/GazFoodSpringBoot-0.1.0-SNAPSHOT.jar user@169.168.9.251:/home/user/app_back/prod
 
--- scripts/deployTest.sh
+++ scripts/deployTest.sh
@@ -8,6 +8,9 @@ mvn clean package
 echo 'Start maven package...'
 mvn package
 
+echo 'Delete old *.jar from dir test'
+ssh user@169.168.9.251 "rm /home/user/app_back/test/GazFoodSpringBoot-0.1.0-SNAPSHOT.jar"
+
 echo 'Copy files to test...'
-scp /home/maxim/IdeaProjects/gazFoodSpringBoot/target/GazFoodSpringBoot-0.1.0-SNAPSHOT.jar user@169.168.9.251:/home/user/tempToTest
+scp /home/maxim/IdeaProjects/gazFoodSpringBoot/target/GazFoodSpringBoot-0.1.0-SNAPSHOT.jar user@169.168.9.251:/home/user/app_back/test
 
